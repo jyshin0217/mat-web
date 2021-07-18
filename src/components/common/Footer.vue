3eb1b6ab2f558492b34c15e12e8b4a5f9fd48aa5
juneyoung shin <5384923-jyshin-0217@users.noreply.gitlab.com>
2021-07-18 22:31:35 +0900

    footer

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,44 +1,92 @@
 <template>
-  <footer>
-      <div class="footer_main">
-          <div class="footer_info_top">
-              <div>쭈니 찌니</div>
-              <div>어쩌구 저쩌구 내용들</div>
-          </div>
-          <div class="footer_info_bottom">
-              <div>
-                  <a href="#"><img alt="kakao logo(icons8)" style="width: 30px" src="../../img/kakao.png" /></a>
-              </div>
-              <div>
-                  <a href="#"><img alt="youtube logo(icons8)" style="width: 30px" src="../../img/youtube.png" /></a>
-              </div>
-              
-
-          </div>
-          
-      </div>
-  </footer>
+  <footer>  
+        <div class="footer-content">
+            <p>술과 음식을 좋아하는 우리가 직접 방문한 맛집들을 소개해주는 블로그입니다.</p>
+            <ul class="socials">
+                <li class="icon s2"><a @click="kakaoShare"><img alt="kakao logo(icons8)" style="width: 30px" src="../../img/kakao.png" /></a></li>
+                <li class="icon s3"><a href="https://www.youtube.com/channel/UCOY2brQ_esbkHEqUEHwPJfg"><img alt="youtube logo(icons8)" style="width: 30px" src="../../img/youtube.png" /></a></li>
+            </ul>
+        </div>
+    </footer>
 </template>
 
 <script>
 export default {
+    name: 'footer',
+    data() {
+        return {
+
+        }
+    },
+    created() {
+    },
+    methods: {
+        kakaoShare() {
+            alert('카카오톡 공유하기')
+        } 
+    }
 
 }
 </script>
 
 <style>
-.footer_main {
-    border: 1px solid blue;
-    display: flex;
-    height: 60px;
+@font-face {
+    font-family: 'jua';
+    src:url('../../../public/fonts/BMJUA_ttf.ttf')
 }
-.footer_info_top {
-    border: 1px solid green;
-    width: 50%;
+@font-face {
+    font-family: 'dohyeon';
+    src:url('../../../public/fonts/BMDOHYEON_ttf.ttf')
 }
-.footer_info_bottom {
-    border: 1px solid red;
-    width: 50%;
+a {
+    cursor:pointer;
+}
+footer{
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background:rgba(114, 113, 113, 0.54);
+    height: auto;
+    width: 100vw;
+    font-family: "Open Sans";
+    padding-top: 10px;
+    color: #111;
+}
+.footer-content{
     display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    font-family:'dohyeon';
+}
+
+.footer-content p{
+    max-width: 500px;
+    margin: 10px auto;
+    line-height: 28px;
+    font-size: 14px;
+}
+.socials{
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 1rem 0 3rem 0;
+}
+.socials li{
+    margin: 0 10px;
+}
+.socials a{
+    text-decoration: none;
+    color: #111;
+}
+.socials a i{
+    font-size: 1.1rem;
+    transition: color .4s ease;
+
+}
+.socials a:hover i{
+    color: aqua;
 }
 </style>
